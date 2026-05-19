4184c1eced15f0b989980864cfcd8b72801b82cd
Aruf Abidzar Atthohiri <arufabidzar.2025@student.uny.ac.id>
2026-05-19 20:32:43 +0700

    Update modul_2.py

--- tbp-asd-kelompok-XX/src/modules/modul_2.py
+++ tbp-asd-kelompok-XX/src/modules/modul_2.py
@@ -1,16 +1,1 @@
-def eval_postfix(tokens, var_table):
-    stack = Stack()
-    for tok in tokens:
-        if tok in FUNCS:
-            arg = stack.pop()
-            stack.push(hitung_fungsi(tok, arg))
-        elif tok in PREC:
-            b = stack.pop()
-            a = stack.pop()
-            stack.push(hitung_operator(tok, a, b))
-        else:
-            # angka atau variabel
-            stack.push(float(tok) if tok not in var_table 
-                       else var_table[tok])
-    return stack.pop()
-                                                                                                                    
+
